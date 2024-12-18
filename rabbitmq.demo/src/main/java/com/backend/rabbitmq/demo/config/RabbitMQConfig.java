4ec5622fd51de21d993a455ffe473d66cfed64fb
antarikshgosain <antariksh.gosain@gmail.com>
2024-12-18 07:23:27 -0500

    bean created for exchange

--- rabbitmq.demo/src/main/java/com/backend/rabbitmq/demo/config/RabbitMQConfig.java
+++ rabbitmq.demo/src/main/java/com/backend/rabbitmq/demo/config/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package com.backend.rabbitmq.demo.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +12,16 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.queue.name}")
     private String queueName;
 
-    //springbean for rabbit queue
-    @Bean
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Bean //spring bean for rabbit queue
     public Queue queue() {
         return new Queue(queueName);
+    }
+    @Bean //spring bean for exchange
+    public TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
     }
 
 }
